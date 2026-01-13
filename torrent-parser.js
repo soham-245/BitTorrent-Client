7e713d282465f9a4d1c23422786c7d45120a070a
soham-245 <sohamnikam2465@gmail.com>
2026-01-13 14:22:03 +0000

    Add size and response type functions

--- torrent-parser.js
+++ torrent-parser.js
@@ -8,9 +8,16 @@ module.exports.open = (filepath) =>{
 };
 
 module.exports.size = torrent=>{
+    const size = torrent.info.files ?
+        torrent.info.files.reduce((sum,file)=>sum+BigInt(files.length),0n)
+        : BigInt(torrent.info.length);
+    const buf = Buffer.alloc(8);
+    buf.writeBigUInt64BE(size,0);
+    return buf;
 
 };
 
 module.exports.infoHash = torrent =>{
-
+    const info = bencode.encode(torrent.info);
+    return crypto.createHash('sha1').update(info).digest();
 };
