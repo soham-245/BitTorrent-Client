30f68b20005b1466aff18291eaa052d33e3de54c
soham-245 <sohamnikam2465@gmail.com>
2026-01-18 13:43:06 +0000

    Add handshake handler

--- src/download.js
+++ src/download.js
@@ -11,14 +11,21 @@ module.exports = torrent => {
 };
 
 function download(peer) {
-  const socket = net.Socket();
+  const socket = new net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
-    // socket.write(...) write a message here
+    socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsg(socket, data => {
-    // handle response here
-  });
+  onWholeMsg(socket, msg => msgHandler(msg, socket));
+}
+
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 &&
+         msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
 
 function onWholeMsg(socket, callback) {
@@ -27,7 +34,7 @@ function onWholeMsg(socket, callback) {
 
   socket.on('data', recvBuf => {
     // msgLen calculates the length of a whole message
-    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readUInt32BE(0) + 4;
     savedBuf = Buffer.concat([savedBuf, recvBuf]);
 
     while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
